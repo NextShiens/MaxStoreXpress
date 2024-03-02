a8e84a65205bf34fde18227d292f93272d47e850
haseeb usman <150597331+haseebusman0305@users.noreply.github.com>
2024-03-02 09:58:40 +0500

    updated customer page

--- src/pages/customers/customerPage.js
+++ src/pages/customers/customerPage.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ProgressBar from "@ramonak/react-progress-bar";
+import { React, useState, useEffect } from 'react';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import StraightIcon from '@mui/icons-material/Straight';
 import Piechart from './chart';
-import CountUp from 'react-countup';
-import { useQuery,gql } from '@apollo/client';
-const GET_DATA=gql`
+import { useQuery, gql } from '@apollo/client';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
+const GET_DATA = gql`
     query{
      customerData{
         all
@@ -19,32 +19,88 @@ const GET_DATA=gql`
         ageOver45
      }   
      }`;
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+    height: 17,
+    borderRadius: 9,
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+        backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+    },
+    [`& .${linearProgressClasses.bar}`]: {
+        borderRadius: 9,
+        backgroundColor: '#00193B',
+    },
+}));
 export default function Customerpage() {
     const { loading, error, data } = useQuery(GET_DATA);
-if(loading) {
+    const [allCus, setAllCus] = useState(0);
+    const [newCus, setNewCus] = useState(0);
+    const [regularCus, setRegularCus] = useState(0);
+    const [ageLess, setageLess] = useState(0);
+    const [age18, setAge18] = useState(0);
+    const [age25, setAge25] = useState(0);
+    const [age45, setAge45] = useState(0);
+
+    useEffect(() => {
+        if (data && data.customerData && allCus < data.customerData.all) {
+            const totalCustomers = data.customerData.all;
+            let increment = 1;
+            if (allCus + 111 <= totalCustomers) {
+                increment = 111;
+               }
+            setAllCus(prev => (prev + increment));
+        }
+        if (data && data.customerData && newCus < data.customerData.new) {
+            const totalCustomers = data.customerData.new;
+            let increment = 1;
+            if (newCus + 111 <= totalCustomers) {
+                increment = 111;
+               }
+            setNewCus(prev => (prev + increment));
+        }
+        if (data && data.customerData && regularCus < data.customerData.regular) {
+            const totalCustomers = data.customerData.regular;
+            let increment = 1;
+            if (regularCus + 111 <= totalCustomers) {
+                increment = 111;
+               }
+            setRegularCus(prev => (prev + increment));
+        }
+        
+        
+if (data && data.customerData && ageLess < data.customerData.ageLess18) {
+    setageLess(prev => prev + 1)
+}
+if (data && data.customerData && age18 < data.customerData.age18_25) {
+    setAge18(prev => prev + 1)
+}
+if (data && data.customerData && age25 < data.customerData.age25_45) {
+    setAge25(prev => prev + 1)
+}
+if (data && data.customerData && age45 < data.customerData.ageOver45) {
+   setAge45(prev => prev + 1)
+}
+    }, [data, ageLess, setageLess, age18, setAge18, age25, setAge25, age45, setAge45,allCus, setAllCus,newCus, setNewCus,regularCus, setRegularCus]);
+if (loading) {
     return <div>Loading Data...</div>;
 }
 if (error) {
     return <div className=' text-red-500 '>Error... </div>;
 }
 if (data) {
-    console.log(data);
     return (
         <div className="pl-7 grid grid-cols-6 gap-y-5 gap-x-[34px] grid-rows-[auto,auto]">
             <div className=' m-7 h-40 mt-16 flex flex-col items-center justify-center text-[22px] w-60 shadow-lg shadow-inset rounded-lg col-span-1  row-span-1 '>
                 <PeopleAltIcon className='text-blue-500 scale-[200%] mb-4' />
-                <CountUp className='text-[22px] ' start={0} end={data.customerData.all} duration={0.9} delay={0.7} />
+                <span className='text-[22px]'>{allCus}</span>
                 All Customers
             </div>
             <div className=' m-7  mt-16 h-40  flex flex-col items-center justify-center text-[22px] w-60 shadow-lg inset-0 shadow-inset rounded-lg col-span-1  row-span-1 '>
                 <PersonAddIcon className='text-pink-600 scale-[200%] mb-4' />
-                <CountUp className='text-[22px] ' start={0} end={data.customerData.new} duration={0.9} delay={0.7} />
-                New Customers
+                <span className='text-[22px]'>{newCus}</span>                New Customers
             </div>
             <div className=' m-7 h-40 mt-16 flex flex-col items-center justify-center text-[22px] w-60 shadow-lg shadow-inset rounded-lg col-span-1  row-span-1'>
                 <AccountCircleIcon className='text-purple-600 scale-[200%] mb-4' />
-                <CountUp className='text-[22px] ' start={0} end={data.customerData.regular} duration={0.9} delay={0.7} />
-                Regular Customers
+                <span className='text-[22px]'>{regularCus}</span>                Regular Customers
             </div>
             <div className='m-7 ml-12 mt-16 gap-y-[13px] gap-x-[13px] h-40  flex flex-col items-center justify-center w-[599px]   shadow-lg shadow-inset rounded-lg col-span-3  row-span-1   '>
                 <h4 className='text-[23px] m-0 font-bold'>Conversion Rate</h4>
@@ -66,7 +122,7 @@ if (data) {
                             </tr>
                             <tr className='m-[6px]'>
                                 <td>2023</td>
-                                <td>+{data.customerData.new}</td>
+                                <td>+{newCus}</td>
                                 <td>$34%</td>
                             </tr>
 
@@ -74,11 +130,11 @@ if (data) {
                     </div>
                     <div className='ml-3 regular-customer-count'>
                         <h1 className='text-[25px] font-bold customer-count-numbers'>
-                            $ <CountUp start={0} end={data.customerData.all} duration={0.9} delay={0.7} />
+                            $ {allCus}
                         </h1>
                     </div>
                     <div className='new-customer-count'>
-                        <h1 className='customer-count-numbers text-[25px] font-bold ml-4'><span className='text-green-600'>+</span><CountUp start={0} end={data.customerData.new} duration={0.9} delay={0.7} />
+                        <h1 className='customer-count-numbers text-[25px] font-bold ml-4'><span className='text-green-600'>+</span>{newCus}
                             <StraightIcon className='text-green-500 pb-[6px] scale-150' />
                         </h1>
                     </div>
@@ -122,51 +178,21 @@ if (data) {
 
                 <p className='font-bold	'>Age 18-25 </p>
 
-                <ProgressBar className='mb-[25px] mt-2'
-                    transitionDuration="1s"
-                    transitionTimingFunction="ease"
-                    animateOnRender
-                    dir="auto"
-                    height="18px"
-                    bgColor="#00193B"
-                    labelSize="13.5px"
-                    completed={data.customerData.ageLess18}
-                    maxCompleted={100} />
+
+                <BorderLinearProgress className='mb-[25px] mt-2' variant="determinate" value={ageLess} />
+
                 <p className='font-bold	'>Age 25-45</p>
-                <ProgressBar className='mb-[25px] mt-2'
-                    transitionDuration="1s"
-                    transitionTimingFunction="ease"
-                    animateOnRender
-                    dir="auto"
-                    height="18px"
-                    bgColor="#00193B"
-                    labelSize="13.5px"
-                    completed={data.customerData.age18_25}
-                    maxCompleted={100} />
+
+                <BorderLinearProgress className='mb-[25px] mt-2' variant="determinate" value={age18} />
                 <p className='font-bold	'>Age over 45 </p>
-                <ProgressBar className='mb-[25px] mt-2'
-                    transitionDuration="1s"
-                    transitionTimingFunction="ease"
-                    animateOnRender
-                    dir="auto"
-                    height="18px"
-                    bgColor="#00193B"
-                    labelSize="13.5px"
-                    completed={data.customerData.age25_45}
-                    maxCompleted={100} />
+
+                <BorderLinearProgress className='mb-[25px] mt-2' variant="determinate" value={age25} />
                 <p className='font-bold	'> Age less 18</p>
-                <ProgressBar className='mb-[12px] mt-2 '
-                    transitionDuration="1s"
-                    transitionTimingFunction="ease"
-                    animateOnRender
-                    dir="auto"
-                    height="18px"
-                    bgColor="#00193B"
-                    labelSize="13.5px"
-                    completed={data.customerData.ageOver45}
-                    maxCompleted={100} />
+
+                <BorderLinearProgress className='mb-[25px] mt-2' variant="determinate" value={age45} />
 
             </div>
         </div>
-    );}
+    );
+}
 }
