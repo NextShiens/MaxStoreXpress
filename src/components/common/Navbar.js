74383d4e2ceceaf2aff0d9f7a36c2e056e0a310b
M Bilal Shafiq <131581977+Bilal713@users.noreply.github.com>
2024-03-12 12:57:50 +0500

    upadate the navbar design and remove some bug without responsive it yet

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -4,7 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { Link as RouterLink, } from 'react-router-dom';
 import { useAuth } from 'react-oidc-context';
 import logo from './logo2.png';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -16,27 +16,26 @@ import LanguageIcon from '@mui/icons-material/Language';
 import { UserManager } from 'oidc-client';
 
 import { oidcConfig } from '../../constant';
+
 const Navbar = () => {
   const { user, isAuthenticated, } = useAuth();
   const [language, setLanguage] = useState('english');
   const [anchorEl, setAnchorEl] = useState(null);
-  const navigate = useNavigate();
   const userManagers = new UserManager(oidcConfig);
+
   const handleLogout = async () => {
-    debugger
-    console.log(userManagers)
+    debugger;
+    console.log(userManagers);
     try {
       await userManagers.signoutRedirect();
     } catch (error) {
       console.error("Error logging out: ", error);
     }
   };
-  useEffect(()=>{
-    if(isAuthenticated){
-    }
-    console.log("user: ", user);  
 
-  },[user,isAuthenticated,navigate ])
+  useEffect(() => {
+    console.log("user: ", user);
+  }, [user]);
 
   const handleLanguageChange = (event) => {
     setLanguage(event.target.value);
@@ -55,155 +54,192 @@ const Navbar = () => {
   const id = open ? 'language-popover' : undefined;
 
   return (
-    <AppBar position="static" color="inherit" sx={{marginTop:"12px",marginRight:"4px" , backgroundColor: '#fff', height: 'auto' }}>
-      <Toolbar sx={{ flexDirection: { xs: 'column', sm: 'row' } }}>
-        <Typography
-          variant="h6"
-          component={RouterLink}
-          to="/"
-          color="inherit"
-          sx={{ color: '#000', flexGrow: 1, textAlign: 'center', mb: { xs: 2, sm: 0 } }}
-        >
-          <img src={logo} alt="Logo" width="200" height="80" />
-        </Typography>
-        <TextField
-          id="outlined-basic"
-          label="Search in maxstore"
-          variant="outlined"
-          sx={{ flexGrow: 1, maxWidth: 500, mb: { xs: 2, sm: 0 } }}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton>
-                  <SearchIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
-        <div style={{ display: 'flex' }}>
-          <IconButton color="inherit" component={RouterLink} to="/cart" sx={{ color: '#000', mr: 1 }}>
-            <ShoppingCartIcon />
-          </IconButton>
-          {isAuthenticated ? (
-             <Button color="inherit" variant="outlined" onClick={handleLogout} sx={{ color: '#000', mr: 1 }}>
-              Logout
-            </Button>
-          ) : (
-            <>
-              <Button
-                component={RouterLink}
-                to="/login"
-                color="inherit"
-                variant="outlined"
-                startIcon={<PersonIcon />}
-                sx={{ color: '#000', mr: 1 }}
-              >
-                Login
-              </Button>
-              <Button
-                component={RouterLink}
-                to="/signup"
-                color="inherit"
-                variant="outlined"
-                startIcon={<AccountCircleIcon />}
-                sx={{ color: '#000', mr: 1 }}
-              >
-                Sign Up
-              </Button>
-            </>
-          )}
-          <div style={{ position: 'relative' }}>
-            <IconButton color="inherit" aria-describedby={id} onClick={handleLanguageIconClick} sx={{ color: '#000', mr: 1 }}>
-              <LanguageIcon />
-            </IconButton>
-            <Popover
-              id={id}
-              open={open}
-              anchorEl={anchorEl}
-              onClose={handleClosePopover}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
+    <>
+      <style>
+        {`
+          .nav-link:hover {
+            color: gray; 
+          }
+
+          .logout-button:hover,
+          .login-button:hover,
+          .signup-button:hover,
+          .icon:hover {
+            background-color: lightgray; 
+          }
+        `}
+      </style>
+      <AppBar position="static" color="inherit" sx={{ backgroundColor: '#fff', height: '120px' }}>
+        <Toolbar>
+          <div style={{ display: 'flex', marginRight: 'auto', marginBottom: '30px' }}>
+            <Typography
+              variant="h6"
+              component={RouterLink}
+              to="/become-seller"
+              color="inherit"
+              sx={{ color: '#000', marginRight: '16px', fontSize: '15px' }}
+              className="nav-link"
             >
-              <List>
-                <ListItem disablePadding>
-                  <FormControlLabel
-                    value="english"
-                    control={<Radio color="primary" />}
-                    label="English"
-                    labelPlacement="start"
-                    checked={language === 'english'}
-                    onChange={handleLanguageChange}
-                    style={{ margin: '5px' }}
-                  />
-                </ListItem>
-                <ListItem disablePadding>
-                  <FormControlLabel
-                    value="urdu"
-                    control={<Radio color="primary" style={{ marginLeft: '20px' }} />}
-                    label="Urdu"
-                    labelPlacement="start"
-                    checked={language === 'urdu'}
-                    onChange={handleLanguageChange}
-                    style={{ margin: '5px' }}
-                  />
-                </ListItem>
-              </List>
-            </Popover>
+              Become a Seller
+            </Typography>
+
+            <Typography
+              variant="h6"
+              component={RouterLink}
+              to="/affiliate-program"
+              color="inherit"
+              sx={{ color: '#000', marginRight: '16px', fontSize: '15px' }}
+              className="nav-link"
+            >
+              MaxStore Affiliate Program
+            </Typography>
+            <Typography
+              variant="h6"
+              component={RouterLink}
+              to="/help-support"
+              color="inherit"
+              sx={{ color: '#000', marginRight: '16px', fontSize: '15px' }}
+              className="nav-link"
+            >
+              Help & Support
+            </Typography>
+
+            <Typography
+              variant="h6"
+              component={RouterLink}
+              to="/logistics-partner"
+              color="inherit"
+              sx={{ color: '#000', marginRight: '16px', fontSize: '15px' }}
+              className="nav-link"
+            >
+              MaxStore Logistics Partner
+            </Typography>
           </div>
-        </div>
-      </Toolbar>
-      <Toolbar  sx={{marginTop:"8px", flexDirection: 'column' }}>
-        <div style={{ display: 'flex', justifyContent: 'center',gap:"15px" }}>
-          <Typography
-         
-            variant="h6"
-            component={RouterLink}
-            to="/become-seller"
-            color="inherit"
-            sx={{ color: '#000', mr: 2, fontSize: '15px'  }}
-          >
-            Become a Seller
-          </Typography>
-
-          <Typography
-            variant="h6"
-            component={RouterLink}
-            to="/affiliate-program"
-            color="inherit"
-            sx={{ color: '#000', mr: 2, fontSize: '15px' }}
-          >
-            MaxStore Affiliate Program
-          </Typography>
-
-          <Typography
-            variant="h6"
-            component={RouterLink}
-            to="/help-support"
-            color="inherit"
-            sx={{ color: '#000', mr: 2, fontSize: '15px' }}
-          >
-            Help & Support
-          </Typography>
-
-          <Typography
-            variant="h6"
-            component={RouterLink}
-            to="/logistics-partner"
-            color="inherit"
-            sx={{ color: '#000', fontSize: '15px' }}
-          >
-            MaxStore Logistics Partner
-          </Typography>
-        </div>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+        <Toolbar>
+          <div style={{ display: 'flex', marginBottom: '10px' }}>
+            <Typography
+              variant="h6"
+              component={RouterLink}
+              to="/"
+              color="inherit"
+              sx={{ flexGrow: 1, color: '#000', marginLeft: '40px', marginBottom: '30px' }}
+              className="logo"
+            >
+              <img src={logo} alt="Logo" width="200" height="80" />
+            </Typography>
+            <TextField
+              id="outlined-basic"
+              label="Search"
+              variant="outlined"
+              sx={{
+                width: '500px',
+                height: '50px',
+                marginTop: '10px',
+                marginLeft: '250px'
+              }}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton>
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+          <div style={{ display: 'flex', marginLeft: '150px' }}>
+            <IconButton color="inherit" className='icon' component={RouterLink} to="/cart" sx={{ color: '#000', marginLeft: '50px', marginBottom: '20px' }}>
+              <ShoppingCartIcon />
+            </IconButton>
+            {isAuthenticated ? (
+              <Button color="inherit" variant="outlined" onClick={handleLogout} sx={{ color: '#000', marginLeft: '30px', marginBottom: '20px' }} className="logout-button">
+                Logout
+              </Button>
+            ) : (
+              <>
+                <Button
+                  component={RouterLink}
+                  to="/login"
+                  color="inherit"
+                  variant="outlined"
+                  startIcon={<PersonIcon />}
+                  sx={{ color: '#000', marginLeft: '30px', marginBottom: '20px' }}
+                  className="login-button"
+                >
+                  Login
+                </Button>
+                <Button
+                  component={RouterLink}
+                  to="/signup"
+                  color="inherit"
+                  variant="outlined"
+                  startIcon={<AccountCircleIcon />}
+                  sx={{ color: '#000', marginLeft: '10px', marginBottom: '20px' }}
+                  className="signup-button"
+                >
+                  Sign Up
+                </Button>
+              </>
+            )}
+            <div style={{ position: 'relative' }}>
+              <IconButton color="inherit" aria-describedby={id} onClick={handleLanguageIconClick} sx={{ color: '#000', marginLeft: '30px' }} className="icon">
+                <LanguageIcon />
+              </IconButton>
+              <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClosePopover}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+              >
+                <List>
+                  <ListItem disablePadding>
+                    <FormControlLabel
+                      value="english"
+                      control={<Radio color="primary" />}
+                      label="English"
+                      labelPlacement="start"
+                      checked={language === 'english'}
+                      onChange={handleLanguageChange}
+                      style={{ margin: '0px 10px' }}
+                    />
+                  </ListItem>
+                  <ListItem disablePadding>
+                    <FormControlLabel
+                      value="urdu"
+                      control={<Radio color="primary" style={{ marginLeft: '20px' }} />}
+                      label="Urdu"
+                      labelPlacement="start"
+                      checked={language === 'urdu'}
+                      onChange={handleLanguageChange}
+                      style={{ margin: '0px 10px' }}
+                    />
+                  </ListItem>
+                </List>
+              </Popover>
+            </div>
+            <IconButton
+              color="inherit"
+              component={RouterLink}
+              to="/cart"
+              sx={{ color: '#000', marginBottom: '20px', marginLeft: '10px' }}
+              className="icon"
+            >
+              <ShoppingCartIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
