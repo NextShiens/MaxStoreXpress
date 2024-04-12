077198012416af4a97bc94660b441a5ec8f5beec
umerfarok <umerfarooq.dev@gmail.com>
2024-04-12 21:52:15 +0500

    Update user preferences mutation and query in ProfileProvider.js

--- src/auth/profileProvider.js
+++ src/auth/profileProvider.js
@@ -2,10 +2,33 @@ import { createContext, useContext, useState, useEffect } from 'react';
 import { useAuth } from 'react-oidc-context';
 import { useMutation, useQuery, gql } from '@apollo/client';
 
+const UPDATE_USER_PREFERENCES = gql`
+  mutation UpdateUserPreferences($id: ID!, $input: UserPreferencesInput) {
+    updateUserPreferences(id: $id, input: $input) {
+      userId
+      id
+      email
+      userName
+      preferredLanguage
+      currencyPreference
+      notificationSettings {
+        newProductUpdates
+        orderUpdates
+        promotionalOffers
+      }
+      createdAt
+      updatedAt
+      lastLoggedIn
+    }
+  }
+`;
+
 const GET_USER_PREFERENCES = gql`
   query getUserPreferencesByEmail($email: String!) {
     getUserPreferencesByEmail(email: $email) {
       id
+      userId
+      userName
       email
       preferredLanguage
       currencyPreference
@@ -16,6 +39,7 @@ const GET_USER_PREFERENCES = gql`
       }
       createdAt
       updatedAt
+      lastLoggedIn
     }
   }
 `;
@@ -24,6 +48,8 @@ const CREATE_USER_PREFERENCES = gql`
   mutation createUserPreferences($input: UserPreferencesInput!) {
     createUserPreferences(input: $input) {
       id
+      userId
+      userName
       email
       preferredLanguage
       currencyPreference
@@ -34,6 +60,7 @@ const CREATE_USER_PREFERENCES = gql`
       }
       createdAt
       updatedAt
+      lastLoggedIn
     }
   }
 `;
@@ -43,54 +70,78 @@ const ProfileContext = createContext({});
 export const useProfile = () => {
   return useContext(ProfileContext);
 };
-
 const ProfileProvider = ({ children }) => {
   const { user, isAuthenticated } = useAuth();
   const [preferencesCreated, setPreferencesCreated] = useState(false);
   const [userPreferences, setUserPreferences] = useState(null);
+  const [updateUserPreferences, { data: updateData, loading: updateLoading, error: updateError }] = useMutation(UPDATE_USER_PREFERENCES);
   const [createUserPreferences, { data: createData, loading: createLoading, error: createError }] = useMutation(CREATE_USER_PREFERENCES);
-  const { data, loading, error } = useQuery(GET_USER_PREFERENCES, {
-    variables: { email: user?.profile?.email },
+  
+  const { data: userPreferencesData, loading: userPreferencesLoading, error: userPreferencesError } =  useQuery(GET_USER_PREFERENCES, {
     skip: !user?.profile?.email,
+    variables: { email: user?.profile?.email },
   });
 
   useEffect(() => {
+    if (userPreferencesData?.getUserPreferencesByEmail) {
+      setUserPreferences(userPreferencesData.getUserPreferencesByEmail);
+    }
+  }, [userPreferencesData]);
+  
+  useEffect(() => {
     const fetchUserPreferences = async () => {
-      if (isAuthenticated && user?.profile?.email) {
-        if (data?.getUserPreferencesByEmail) {
-          setUserPreferences(data.getUserPreferencesByEmail);
-        } else if (error && error.message === 'User preferences not found' && !preferencesCreated) {
-          await createUserPreferences({
-            variables: {
-              input: {
-                email: user.profile.email,
-                preferredLanguage: 'en',
-                currencyPreference: 'USD',
-                notificationSettings: {
-                  newProductUpdates: true,
-                  orderUpdates: true,
-                  promotionalOffers: true,
-                },
-              },
+      if (isAuthenticated && user?.profile?.email && !userPreferencesLoading) {
+        try {
+          const userPreferencesInput = {
+            userId: user.profile.sub,
+            userName: user.profile['cognito:username'],
+            email: user.profile.email,
+            preferredLanguage: 'en',
+            currencyPreference: 'USD',
+            notificationSettings: {
+              newProductUpdates: true,
+              orderUpdates: true,
+              promotionalOffers: true,
             },
-          });
-          setUserPreferences(createData?.createUserPreferences);
-          setPreferencesCreated(true);
+            lastLoggedIn: new Date().toISOString(),
+          };
+  
+          let userPreferencesResult;
+          if (userPreferencesData?.getUserPreferencesByEmail) {
+            userPreferencesResult = await updateUserPreferences({
+              variables: {
+                id: userPreferencesData.getUserPreferencesByEmail.id,
+                input: userPreferencesInput,
+              },
+            });
+          } else if (!preferencesCreated) {
+            userPreferencesResult = await createUserPreferences({
+              variables: {
+                input: userPreferencesInput,
+              },
+            });
+            setPreferencesCreated(true);
+          }
+  
+          setUserPreferences(userPreferencesResult?.data?.createUserPreferences || userPreferencesResult?.data?.updateUserPreferences);
+        } catch (error) {
+          console.error('Error fetching user preferences:', error);
+          // Handle the error, e.g., display an error message to the user
         }
       }
     };
-
+  
     fetchUserPreferences();
-  }, [isAuthenticated, user, data, createUserPreferences, createData, error]);
-
+  }, [isAuthenticated, user, userPreferencesLoading]);
 
   return (
     <ProfileContext.Provider
       value={{
         profile: user?.profile || {},
         userPreferences,
-        loading,
         createLoading,
+        updateLoading,
+        createError,
       }}
     >
       {children}
